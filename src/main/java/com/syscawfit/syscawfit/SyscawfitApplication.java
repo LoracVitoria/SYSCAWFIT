281de2397ba8829aca539a95f93cd109b490e9c9
angelicacampos <71331299+AngelicaCamp@users.noreply.github.com>
2022-06-27 11:27:05 -0300

    Initial Commit

---
+++ src/main/java/com/syscawfit/syscawfit/SyscawfitApplication.java
@@ -0,0 +1,13 @@
+package com.syscawfit.syscawfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyscawfitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyscawfitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/syscawfit/syscawfit/SyscawfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syscawfit.syscawfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyscawfitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
