d8ab10743f3c671a0d92bbd0f3391ed5a5bbbf6a
William Ricardo Munaretto <william.munaretto@gmail.com>
2022-07-25 14:35:15 -0300

    1 - alteração da EquipamentosController 2 - Alteração da TipoExercicioController 3 - Alteração de TipoExercicio.java (model) 4 - Alteração de equipamentos.css 5 - Alteração de exercicios.css 6 - implementaçao de equipamentos.js 7 - implementação de framework uikit (teste para view de imagens do equipamentos) 8 - modificado layout de aluno.html (apenas detalhes de posicionamento da itens na pagina) 9 - modificado cadastrarEquipamentos.html (conforme layout pagina aluno.html) 10 - renomeado consultarEquipamentos.html para listarEquipamentos.html 11 - modificado cadastrarTipoExercicio.html (em teste - não finalizado) 12 - modificado listaTipoExercicio.html (layout) 13 - modificado menu.html (retirado o dropown de exercicios e equipamentos para padronizar conforme Aluno) 14 - modificado equipametos.css (ajuste de classes conforme cadastrarallunos.css e consultarAlunos.css)

--- src/main/java/com/syscawfit/syscawfit/controller/TipoExercicioController.java
+++ src/main/java/com/syscawfit/syscawfit/controller/TipoExercicioController.java
@@ -12,60 +12,64 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+import com.syscawfit.syscawfit.dao.EquipamentosRepository;
 import com.syscawfit.syscawfit.dao.TipoExercicioRepository;
+import com.syscawfit.syscawfit.model.Equipamentos;
 import com.syscawfit.syscawfit.model.TipoExercicio;
 
 @Controller
 @RequestMapping("/tipoexercicio")
 public class TipoExercicioController {
 
-	
 	@Autowired
 	private TipoExercicioRepository daoExercicio;
-	
+
+	@Autowired
+	private EquipamentosRepository daoEquipamento;
+
 	/* NOVO TIPO_EXERCICIO */
 	@RequestMapping("/new")
 	public String novoTipoExercicio(Model model) {
 		TipoExercicio tipoExercicio = new TipoExercicio();
-		model.addAttribute("tipoExercicio", tipoExercicio );
-		
+		List<Equipamentos> equipamentosList = daoEquipamento.findAll();
+		model.addAttribute("tipoExercicio", tipoExercicio);
+		model.addAttribute("equipamentosList", equipamentosList);
 
 		return "/exercicios/cadastrarTipoExercicio.html";
 	}
-	
+
 	/* SALVAR TIPO_EXERCICIO */
 	@PostMapping("/save")
 	public String save(@Valid TipoExercicio tipoExercicio, BindingResult result, Model model) {
 		if (result.hasErrors()) {
 			return "redirect:/exercicios/cadastrarTipoExercicio.html";
 		}
-		
+
 		daoExercicio.save(tipoExercicio);
-		
+
 		return "redirect:/tipoexercicio/list";
 	}
-	
+
 	/* UPDATE TIPO_EXERCICIO */
 	@PostMapping("/update")
 	public String update(TipoExercicio tipoExercicio, Model model) {
-		daoExercicio.save(tipoExercicio); 	
+		daoExercicio.save(tipoExercicio);
 		return "redirect:/tipoexercicio/list";
 	}
-	
+
 	/* DELETE TIPO_EXERCICIO */
 	@RequestMapping("/delete/{id}")
 	public String delete(Model model, @PathVariable Long id) {
-		daoExercicio.deleteById(id); 	
+		daoExercicio.deleteById(id);
 		return "redirect:/tipoexercicio/list";
 	}
-	
-	/*LISTA TODOS EQUIPAMENTOS*/
+
+	/* LISTA TODOS EQUIPAMENTOS */
 	@RequestMapping("/list")
 	public String list(Model model) {
 		List<TipoExercicio> tipoExercicioLista = daoExercicio.findAll();
 		model.addAttribute("tipoExercicioLista", tipoExercicioLista);
-		
+
 		return "/exercicios/listaTipoExercicio.html";
 	}
 }
