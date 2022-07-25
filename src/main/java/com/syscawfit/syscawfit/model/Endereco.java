bbe622d7cc05601ca936810855677d308e6a9f6d
LoracVitoria <carolvitoria1705@gmail.com>
2022-07-25 10:25:14 -0300

    rebase angel

--- src/main/java/com/syscawfit/syscawfit/model/Endereco.java
+++ src/main/java/com/syscawfit/syscawfit/model/Endereco.java
@@ -1,15 +1,7 @@
 package com.syscawfit.syscawfit.model;
 
-import lombok.*;
-
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 @Entity
 public class Endereco {
 
@@ -17,15 +9,75 @@ public class Endereco {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String cep;
-    private String logradouro;
+    private String rua;
     private Long numero;
     private String bairro;
     private String cidade;
     private String uf;
     private String pais;
 
-    @NotNull
-    @ManyToOne(cascade = CascadeType.REMOVE)
-    private Aluno aluno;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUF(String uf) {
+        this.uf = uf;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
 }
