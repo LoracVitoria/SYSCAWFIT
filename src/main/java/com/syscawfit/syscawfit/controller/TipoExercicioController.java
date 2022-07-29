56f526b5ea6a7925a98688cf8fd200f4e514c72d
William Ricardo Munaretto <william.munaretto@gmail.com>
2022-07-29 16:37:37 -0300

    1 - modificado TipoExercicioController.java 2 - criado TreinoController.java 3 - criado TreinoRepository 4 - modificado TipoExercicio.java 5 - criado Treino.java (model) 6 - criado treino.css 7 - criado treino.js 8 - modiicado listaTipoExercicio.html 9 - modificado menu.htmll 10 - criado cadastrarTreino.html 11 - criado listarTreinos.html 12 - testado persistencia no banco de dados 13 - CRUD treino com new, listAll, save e delete funcionando.

--- src/main/java/com/syscawfit/syscawfit/controller/TipoExercicioController.java
+++ src/main/java/com/syscawfit/syscawfit/controller/TipoExercicioController.java
@@ -81,4 +81,7 @@ public class TipoExercicioController {
 
 		return "/exercicios/listaTipoExercicio.html";
 	}
+
+	
+	
 }
---
+++ src/main/java/com/syscawfit/syscawfit/dao/TreinoRepository.java
@@ -0,0 +1,10 @@
+package com.syscawfit.syscawfit.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.syscawfit.syscawfit.model.Treino;
+
+
+public interface TreinoRepository extends JpaRepository<Treino, Long>{
+
+}
