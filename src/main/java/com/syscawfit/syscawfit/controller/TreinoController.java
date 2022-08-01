d6c30c4f7375b569ab013b7298cb9b9a0db5b28a
William Ricardo Munaretto <william.munaretto@gmail.com>
2022-08-01 18:16:34 -0300

    1 - inserido ExercicioController.java 2 - inserido enter.java (model) 3 - modificado TreinoController.java.

--- src/main/java/com/syscawfit/syscawfit/controller/TreinoController.java
+++ src/main/java/com/syscawfit/syscawfit/controller/TreinoController.java
@@ -69,6 +69,8 @@ public class TreinoController {
 			return "redirect:/treino/list";
 		}
 
+		
+
 
 
 }
--- src/main/java/com/syscawfit/syscawfit/model/Exercicio.java
+++ src/main/java/com/syscawfit/syscawfit/model/Exercicio.java
@@ -1,6 +1,11 @@
 package com.syscawfit.syscawfit.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -16,9 +21,9 @@ public class Exercicio {
 	@NotBlank(message = "campo obrigatório")
 	@Size(min = 3, max = 50, message = "Campo deve conter entre {min} e {max} carácteres")
 	private String nome;
-
-	@ManyToOne
-	@JoinColumn(name = "tipo_exercicio_id")
+	
+	@OneToMany
+	@JoinColumn(name = "id_equipamento")
 	private TipoExercicio tipoExercicio;
 	
 	private Integer qtdeRepeticoes;
