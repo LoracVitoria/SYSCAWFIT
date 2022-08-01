68c9395b69b292e2646dd70c7fd8d8f50f000364
angelicacampos <71331299+AngelicaCamp@users.noreply.github.com>
2022-08-01 16:27:54 -0300

    realizado crud para aluno e endereço

--- src/main/java/com/syscawfit/syscawfit/SyscawfitApplication.java
+++ src/main/java/com/syscawfit/syscawfit/SyscawfitApplication.java
@@ -4,6 +4,7 @@ package com.syscawfit.syscawfit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
--- src/main/resources/static/js/aluno/mascaras.js
+++ src/main/resources/static/js/aluno/mascaras.js
@@ -1,7 +1,21 @@
 
-$( "#cpf").blur(function() {
+// Máscara para CPF
+$("#cpf").keypress(function() {
     $(this).mask('000.000.000-00');
 });
+
+// Máscara para telefone
+$("#telefone").keypress(function() {
+    $(this).mask('(00) 00000-0000')
+});
+
+// Máscara para CPF
+$("#cpfBusca").keypress(function() {
+    $(this).mask('000.000.000-00');
+});
+
+
+
 
 
  
