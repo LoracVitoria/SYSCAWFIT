5d3aa8c3a4c24d4c367f629d209e82e5105eabb2
William Ricardo Munaretto <william.munaretto@gmail.com>
2022-08-05 17:28:57 -0300

    1 - modificado listarEquipamentos.html 2 - retirado o medoto de criar pasta de dentro do SyscawfitApplication.class

--- src/main/java/com/syscawfit/syscawfit/SyscawfitApplication.java
+++ src/main/java/com/syscawfit/syscawfit/SyscawfitApplication.java
@@ -1,19 +1,16 @@
 package com.syscawfit.syscawfit;
 
-import java.io.File;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.syscawfit.syscawfit.controller.EquipamentosController;
 
 @SpringBootApplication
 public class SyscawfitApplication {
 
     public static void main(String[] args) {
     	
-    	//cria pasta dentro do sistema para recebimento de imagens conforme o caminho especificado.
-//    	new File(EquipamentosController.caminhoImagens).mkdir();
     	
         SpringApplication.run(SyscawfitApplication.class, args);
     }
