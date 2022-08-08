9b869a79f6da1d1652296c19fe4e31f9f74f4d96
angelicacampos <71331299+AngelicaCamp@users.noreply.github.com>
2022-08-08 18:58:00 -0300

    Entidades Aula e Plano adicionadas

---
+++ src/main/java/com/syscawfit/syscawfit/model/Plano.java
@@ -0,0 +1,31 @@
+package com.syscawfit.syscawfit.model;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+@Entity
+public class Plano {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Column(unique = true)
+    @Enumerated(EnumType.STRING)
+    private TipoPlano tipo;
+
+    @NotNull
+    private float valor;
+
+}
