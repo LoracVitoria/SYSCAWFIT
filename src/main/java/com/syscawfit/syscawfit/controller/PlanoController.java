39b5139d2f4cee7d1297bf2feccfec2eb86280a5
angelicacampos <71331299+AngelicaCamp@users.noreply.github.com>
2022-08-09 17:47:11 -0300

    concluído estilo para página de Plano

--- src/main/java/com/syscawfit/syscawfit/controller/PlanoController.java
+++ src/main/java/com/syscawfit/syscawfit/controller/PlanoController.java
@@ -9,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 
 @Controller
 @RequestMapping("/planos")
@@ -17,15 +19,16 @@ public class PlanoController {
     @Autowired
     private PlanoRepository planoDao;
 
-
     // Retorna página de planos
     @RequestMapping()
     public String novoPlano(Model model){
 
         Plano plano = new Plano();
+        List<Plano> planos = planoDao.findAll();
 
         model.addAttribute("plano",plano);
-        model.addAttribute("planos", TipoPlano.values());
+        model.addAttribute("planos",planos);
+        model.addAttribute("tiposPlanos", TipoPlano.values());
 
         return "/planos/plano";
     }
@@ -33,6 +36,15 @@ public class PlanoController {
     // Salvar plano
     @PostMapping("/save")
     public String salvarAula(Plano plano, Model model){
+        //buscar no banco plano por tipo, caso não seja encontrado retornar nulo
+        Plano planoExistente = planoDao.findPlanoByTipo(plano.getTipo()).orElse(null);
+
+        // verificar se busca encontrou plano, se encontrou setar para plano o ID do planoExistente encontrado
+        if (planoExistente != null) {
+            plano.setId(planoExistente.getId());
+        }
+
+        // salvar caso planoExistente seja null
         planoDao.save(plano);
 
         return "redirect:/planos";
