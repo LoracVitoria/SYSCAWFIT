e5ed1001023a7ae16232061299f49a5f3dad9c60
Angélica Campos <angelicacampos.dev@gmail.com>
2022-08-10 10:39:40 -0300

    rebase main

--- src/main/java/com/syscawfit/syscawfit/SyscawfitApplication.java
+++ src/main/java/com/syscawfit/syscawfit/SyscawfitApplication.java
@@ -1,18 +1,13 @@
 package com.syscawfit.syscawfit;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
 
 @SpringBootApplication
 public class SyscawfitApplication {
 
     public static void main(String[] args) {
-    	
-    	
         SpringApplication.run(SyscawfitApplication.class, args);
     }
 
