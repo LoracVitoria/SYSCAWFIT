4cdbc6039fbd8986ed8b7023d0fefc407a3c6c21
LoracVitoria <carolvitoria1705@gmail.com>
2022-08-10 19:48:34 -0300

    atualizções

--- src/main/java/com/syscawfit/syscawfit/model/Usuario.java
+++ src/main/java/com/syscawfit/syscawfit/model/Usuario.java
@@ -8,8 +8,26 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String nome;
+    private String CPF;
+    private String RG;
+    private String telefone;
+    @OneToOne
+    @JoinColumn(name = "endereco_id")
+    private Endereco endereco;
+    private String email;
+    private Boolean situacao;
+    private Boolean mantenedor;
+    private String senha;
+
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 
 
     public Long getId() {
@@ -28,8 +46,59 @@ public class Usuario {
         this.nome = nome;
     }
 
+    public String getCPF() {
+        return CPF;
+    }
 
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
 
+    public String getRG() {
+        return RG;
+    }
 
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
 
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(Boolean situacao) {
+        this.situacao = situacao;
+    }
+
+    public Boolean getMantenedor() {
+        return mantenedor;
+    }
+
+    public void setMantenedor(Boolean mantenedor) {
+        this.mantenedor = mantenedor;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
