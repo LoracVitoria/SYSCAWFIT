d63401528fd775c9c70e87bc8472871c73c99cb6
Angélica Campos <angelicacampos.dev@gmail.com>
2022-08-10 18:52:29 -0300

    alterado relacionamento entre Aluno e Endereço

--- src/main/java/com/syscawfit/syscawfit/controller/UsuarioController.java
+++ src/main/java/com/syscawfit/syscawfit/controller/UsuarioController.java
@@ -1,9 +1,12 @@
 package com.syscawfit.syscawfit.controller;
 
+import com.syscawfit.syscawfit.dao.UsuarioDao;
 import com.syscawfit.syscawfit.model.Aluno;
 import com.syscawfit.syscawfit.model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,20 +14,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UsuarioController {
 
 
-        //CONSULTAR USUARIO
-        @RequestMapping("/list")
-        public String buscar(Model model) {
-            Usuario usuario = new Usuario();
-            model.addAttribute("usuario", usuario );
-            return "/usuario/list.html";
-        }
+    @Autowired
+    private UsuarioDao usuarioDao;
 
-        // NOVO USUARIO
-        @RequestMapping("/new")
-        public String cadastrar(Model model){
-            Usuario usuario = new Usuario();
-            model.addAttribute("usuario", usuario );
-            return "/usuario/manter-usuario.html";
-        }
 
+    //CONSULTAR USUARIO
+    @RequestMapping("/list")
+    public String buscar(Model model) {
+        Usuario usuario = new Usuario();
+        model.addAttribute("usuario", usuario );
+
+        return "/usuario/list.html";
     }
+
+    // NOVO USUARIO
+    @RequestMapping("/new")
+    public String cadastrar(Model model){
+        Usuario usuario = new Usuario();
+        model.addAttribute("usuario", usuario );
+
+        return "/usuario/usuario.html";
+    }
+
+    @PostMapping("/save")
+    public String salvarUsuario(Usuario usuario, Model model){
+        usuarioDao.save(usuario);
+
+        return "redirect:/usuario/list";
+    }
+
+
+
+
+}
