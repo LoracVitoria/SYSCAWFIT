90e8bd7b1acdf1bced4af0c1c80788d374a4cc5f
Angélica Campos <angelicacampos.dev@gmail.com>
2022-08-15 18:18:19 -0300

    commit

--- src/main/java/com/syscawfit/syscawfit/model/AulaDiaHora.java
+++ src/main/java/com/syscawfit/syscawfit/model/AulaDiaHora.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import javax.persistence.Embeddable;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalTime;
 
@@ -14,10 +13,8 @@ public class AulaDiaHora implements Serializable {
 
     private static final long serialVersionUID = 3067070130719740930L;
 
-    @NotNull
     @Enumerated(EnumType.STRING)
     private DiaSemana diaSemana;
 
-    @NotNull
     private LocalTime hora;
 }
