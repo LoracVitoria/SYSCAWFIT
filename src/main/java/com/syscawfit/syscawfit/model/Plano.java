4e3367f15bf640bb810b97bb5010ee148ba24a65
Angélica Campos <angelicacampos.dev@gmail.com>
2022-08-16 18:57:19 -0300

    realizado tratamento de erro

--- src/main/java/com/syscawfit/syscawfit/model/Plano.java
+++ src/main/java/com/syscawfit/syscawfit/model/Plano.java
@@ -4,6 +4,8 @@ package com.syscawfit.syscawfit.model;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 
 @Getter
@@ -19,10 +21,12 @@ public class Plano {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+//    @NotEmpty(message = "O campo tipo de plano não deve ser vazio.")
     @Column(unique = true)
     @Enumerated(EnumType.STRING)
     private TipoPlano tipo;
 
+//    @NotEmpty(message = "O campo valor não deve ser vazio.")
     private float valor;
 
 }
