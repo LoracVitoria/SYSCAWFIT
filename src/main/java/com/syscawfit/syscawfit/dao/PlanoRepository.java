fcd5b3f7d405d363683640dd0233159bc5fec371
Angélica Campos <angelicacampos.dev@gmail.com>
2022-08-16 19:51:04 -0300

    rebase main

---
+++ src/main/java/com/syscawfit/syscawfit/dao/PlanoRepository.java
@@ -0,0 +1,7 @@
+package com.syscawfit.syscawfit.dao;
+
+import com.syscawfit.syscawfit.model.Plano;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanoRepository extends JpaRepository<Plano,Long> {
+}
