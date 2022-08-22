1037a3817858ab41514be075fd02f836473605d5
Angélica Campos <angelicacampos.dev@gmail.com>
2022-08-22 15:11:14 -0300

    rebase

--- src/main/java/com/syscawfit/syscawfit/controller/PlanoController.java
+++ src/main/java/com/syscawfit/syscawfit/controller/PlanoController.java
@@ -6,14 +6,8 @@ import com.syscawfit.syscawfit.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.validation.ConstraintViolationException;
-import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Controller
@@ -23,49 +17,22 @@ public class PlanoController {
     @Autowired
     private PlanoRepository planoDao;
 
+
     // Retorna página de planos
     @RequestMapping()
     public String novoPlano(Model model){
 
         Plano plano = new Plano();
-        List<Plano> planos = planoDao.findAll();
 
         model.addAttribute("plano",plano);
-        model.addAttribute("planos",planos);
-        model.addAttribute("tiposPlanos", TipoPlano.values());
+        model.addAttribute("planos", TipoPlano.values());
 
         return "/planos/plano";
     }
 
     // Salvar plano
     @PostMapping("/save")
-    public String salvarAula(@Valid Plano plano, BindingResult result, Model model){
-
-        if (result.hasErrors()) {
-            List<String> errors = new ArrayList<>();
-            result.getAllErrors().forEach(error -> {
-                errors.add(error.getDefaultMessage());
-            });
-
-            List<Plano> planos = planoDao.findAll();
-
-            model.addAttribute("plano",plano);
-            model.addAttribute("planos",planos);
-            model.addAttribute("tiposPlanos", TipoPlano.values());
-            model.addAttribute("mensagensErro", errors);
-
-            return "/planos/plano";
-        }
-
-        //buscar no banco plano por tipo, caso não seja encontrado retornar nulo
-        Plano planoExistente = planoDao.findPlanoByTipo(plano.getTipo()).orElse(null);
-
-        // verificar se busca encontrou plano, se encontrou setar para plano o ID do planoExistente encontrado
-        if (planoExistente != null) {
-            plano.setId(planoExistente.getId());
-        }
-
-        // salvar caso planoExistente seja null
+    public String salvarAula(Plano plano, Model model){
         planoDao.save(plano);
 
         return "redirect:/planos";
--- src/main/java/com/syscawfit/syscawfit/model/Plano.java
+++ src/main/java/com/syscawfit/syscawfit/model/Plano.java
@@ -4,9 +4,6 @@ package com.syscawfit.syscawfit.model;
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 
@@ -23,12 +20,12 @@ public class Plano {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotNull(message = "O campo tipo de plano deve ser preenchido.")
+    @NotNull
     @Column(unique = true)
     @Enumerated(EnumType.STRING)
     private TipoPlano tipo;
 
-    @Min(value = 10,message = "O valor de ser maior ou igual a 10.")
+    @NotNull
     private float valor;
 
 }
